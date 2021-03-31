bfed844e86776d8ec11ca51324ea02bd955c12dc
Manish Poduval <manishpoduval@hotmail.com>
2021-03-31 15:43:33 +0530

    async practice

--- timer.js
+++ timer.js
@@ -1,13 +1,24 @@
 // add properties and methods for your timer here
 class Timer {
     //STEP 0: Add necessary properties for your timer
+    constructor(){
+        this.time = 0
+        this.intervalId = 0
+    }
+    start(updateFunc){
+        console.log('Start called')
+        this.intervalId = setInterval(() => {
+            this.time++
 
-
-    // STEP1: Create a start method to start the timer
-
-
-
-    // STEP2: Create a stop method to stop the timer
-
-
+           updateFunc()
+        }, 1000)
+    }
+    stop(){
+        clearInterval(this.intervalId)
+    }
 }
+
+
+
+
+
